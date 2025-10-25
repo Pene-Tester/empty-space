a1b3fd958491511424e95cb89c893b7c479ed0a9
John Phavel <brokored@gmail.com>
2025-10-25 22:11:02 +0800

    deployment testing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +20,6 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use Railway's provided PORT environment variable
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
